5a986a3254101c178078fdfbb45cc287eda70fb6
“DanielaP” <padronjodaniela@gmail.com>
2023-09-19 12:13:41 -0500

    agregarimagenes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import { example } from './dataFunctions.js';
-import { renderItems } from './view.js';
+import data from './data/got/got.js';
+const imagenes = document.getElementById('imagenes')
+const personajes = document.getElementById('Personajes')
 
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, renderItems, data);
+export const data = () => {
+    const images = data.got.map((item) => {
+        const element = document.createElement(img);
+        img.src = item.imageUrl;
+        img.alt = item.fullName;
+        img.width = 100;
+        img.heigth = 100;
+        return img;
+
+    })
+    imagesMovies.map((image) => allMovies.appendChild(image));
+
+};
--- src/view.js
+++ src/view.js
@@ -1,4 +1,6 @@
 // estas funciones son de ejemplo
+const personajes = document.getElementById('Personajes')
+
 
 export const renderItems = () => {
   return 'example';
