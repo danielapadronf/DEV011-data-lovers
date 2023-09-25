7205b3d81043d56f2c5bc5f0e1c8284b29a8a981
Jesycl <142137601+Jesycl@users.noreply.github.com>
2023-09-25 08:40:18 -0600

    avance de pagina y nueva carpeta de imagenes

--- src/view.js
+++ src/view.js
@@ -1,8 +1,22 @@
+import data from './data/got/got.js';
 // estas funciones son de ejemplo
-const personajes = document.getElementById('Personajes')
+//const personajes = document.getElementById('Personajes')
 
+const listaPersonajes = data.got;
+const contenedorPersonajes = document.querySelector('#root');
 
-export const renderItems = () => {
-  return 'example';
+const monstrarCartasPersonajes = (list) => {
+  const ul = document.createElement('ul');
+  list.forEach((personaje) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <div class="personaje-img">
+        <img src="${personaje.imageUrl}">
+        <p class="personaje-nombre">${personaje.firstName}</p>
+      </div>`;
+    ul.appendChild(li);
+  });
+
+  contenedorPersonajes.appendChild(ul);
 };
-
+monstrarCartasPersonajes(listaPersonajes);
